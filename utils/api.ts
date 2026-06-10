23bba33b90e1ac07a994f843ccd62a9d929931f1
shar-jina <sharjinaraseena@gmail.com>
2026-06-10 13:38:49 +0530

    add

--- utils/api.ts
+++ utils/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:5001";
-// export const API_BASE_URL = "https://childcraft-backend-ftlm.onrender.com";
+// export const API_BASE_URL = "http://localhost:5001";
+export const API_BASE_URL = "https://childcraft-backend-ftlm.onrender.com";
